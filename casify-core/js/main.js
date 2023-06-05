b0a538f15fa94fe7f541dd8b9ca12e061e0a40ba
asaghir <asaghir@3dcart.com>
2023-06-05 08:36:29 -0400

    DFN-2077

--- casify-core/js/main.js
+++ casify-core/js/main.js
@@ -92,7 +92,7 @@ function moveMenu() {
         respWidth = jQuery(window).width();
     }
 
-    if (respWidth < 767) {
+    if (respWidth < 768) {
         jQuery('#menulinks').appendTo('#mobile-menulinks');
         jQuery('#categories').appendTo('#mobile-categories');
     }
@@ -125,7 +125,7 @@ jQuery(document).ready(function () {
 
 
     var respWidth = window.innerWidth;
-    if (respWidth >= 767) {
+    if (respWidth >= 768) {
     	jQuery('.navbar .dropdown').hover(function () {
     		jQuery(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown('fast');
 
@@ -176,3 +176,6 @@ jQuery(function () {
         }
     });
 });
+setTimeout(function(){
+      $('.antiCls, .antiCls-xs').css("visibility", "visible");
+  }, 800);
